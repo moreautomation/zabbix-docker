168ab946f59f86f176c20916a6a0aeb7ae436c8c
Marko Kundacina <marko@moreautomation.com>
2025-01-30 19:04:32 -0500

    Added branding

---
+++ brand.conf.php
@@ -0,0 +1,19 @@
+<?php
+return [
+   // Logo displayed on the login screen (recommended size 114x30px)
+   'BRAND_LOGO' => './branding/MoreAutomation_Zabbix_Logo_Color_146x30.svg',
+
+   // Logo in the left sidebar menu (recommended size 91x24px)
+   'BRAND_LOGO_SIDEBAR' => './branding/MoreAutomation_Zabbix_Logo_White_117x24.svg',
+
+   // Symbol for compact menu mode (recommended size 24x24px)
+   'BRAND_LOGO_SIDEBAR_COMPACT' => './branding/MoreAutomation_Zabbix_Symbol_White_24x24.svg',
+
+   /* Text displayed in the page footer
+      Can contain company name or copyright information */
+   'BRAND_FOOTER' => 'More Automation Solutions Inc.',
+   
+   /* URL that opens when clicking the help button
+      Can point to your own documentation or support page */
+   'BRAND_HELP_URL' => 'https://www.moreautomation.com/'
+];
